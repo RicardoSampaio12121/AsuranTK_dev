3007a8e087403bdc5c3f1aec97b16ba8e763e429
Ricardo Sampaio <ricardo_cs@outlook.pt>
2021-07-27 12:30:19 +0100

    Api connector implemented.

---
+++ GW2Wrapper/Connector/Connector.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.Http;
+
+namespace GW2Wrapper.Connector
+{
+    /// <summary>
+    /// Class of the api connector
+    /// </summary>
+    class Connector : IConnector
+    {
+        private readonly string _apiKey;
+        private readonly HttpClient _client;
+        private const string DefaultUrl = @"https://api.guildwars2.com/";
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="apiKey"></param>
+        public Connector(string apiKey, HttpClient client)
+        {
+            _apiKey = apiKey;
+            _client = client;
+        }
+
+        public string Call(string endpoint)
+        {
+            string requestUrl = $@"{DefaultUrl}{endpoint}";
+            var stringTask = _client.GetAsync(requestUrl);
+            stringTask.Wait();
+            var output = stringTask.Result;
+            return output.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
---
+++ GW2Wrapper/Connector/IConnector.cs
@@ -0,0 +1,17 @@
+﻿namespace GW2Wrapper.Connector
+{
+    /// <summary>
+    /// Interface of the api connector
+    /// </summary>
+    interface IConnector
+    {
+        /// <summary>
+        /// Calls an endpoint of the api
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <returns>
+        /// Content in the endpoint
+        /// </returns>
+        string Call(string endpoint);
+    }
+}
